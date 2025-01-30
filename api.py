6f9a439415ebff2fe0206dc15e47ff17fa729a4c
pams90 <101939525+pams90@users.noreply.github.com>
2025-01-30 19:02:35 +0200

    Update api.py

--- api.py
+++ api.py
@@ -108,4 +108,4 @@ def get_background_sounds():
 
 if __name__ == '__main__':
     print("Starting Flask API server...")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
